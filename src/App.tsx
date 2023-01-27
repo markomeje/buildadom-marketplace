6b89147c5fedc3d0caeacab39b5f40e217a2a4a6
markomeje <markomejeonline@gmail.com>
2023-01-27 12:00:51 +0100

    App.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -20,13 +20,8 @@ export default function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <h1>Buildadom Marketplace</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
