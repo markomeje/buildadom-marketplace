78c6c517f09b9a67d60f7ab1df322626ab4f0aec
markomeje <markomejeonline@gmail.com>
2023-01-27 12:21:04 +0100

    App.tsx Updated

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,8 @@ export default function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <h1>Buildadom Marketplace</h1>
+      <div className="">
+        <h2>Buildadom Marketplace Live on AWS Amplify</h2>
       </div>
     </div>
   )
